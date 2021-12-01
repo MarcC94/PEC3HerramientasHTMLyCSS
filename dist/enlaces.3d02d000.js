8ed19c02e0456acfa6b32c337e0cc8e356881149
MarcC94 <mcanaba@uoc.edu>
2021-12-01 17:40:21 +0100

    changes

---
+++ dist/enlaces.3d02d000.js
@@ -0,0 +1,2 @@
+document.getElementById("banner").onclick=function(){alert("¡Viaja ya, no te lo pienses!")};
+//# sourceMappingURL=enlaces.3d02d000.js.map
---
+++ dist/enlaces.bb2cf4be.js
@@ -0,0 +1,2 @@
+document.getElementById("banner").onclick=function(){alert("¡Viaja ya, no te lo pienses!")};
+//# sourceMappingURL=enlaces.bb2cf4be.js.map
